03a3ad6525829b672fd589f706d4b487da01210f
Mobashir Monim <mobashirmonim@gmail.com>
2022-08-13 23:59:03 +0600

    Update secondary button style

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -11,10 +11,13 @@ export const bgColorStyles = {
 
 export const buttonStyles = {
     primary: "bg-blue-600 text-white hover:bg-rose-600 transition-all duration-300 ease-linear rounded-xl px-4 py-2 drop-shadow-md hover:drop-shadow-2xl",
-    secondary: "bg-orange-600 text-white hover:bg-lime-600 transition-all duration-300 ease-linear rounded-xl px-4 py-2 drop-shadow-md hover:drop-shadow-2xl"
+    secondary: "bg-emerald-600 text-white hover:bg-indigo-600 transition-all duration-300 ease-linear rounded-xl px-4 py-2 drop-shadow-md hover:drop-shadow-2xl"
 }
 
 export const cardStyles = {
     simple: `p-5 drop-shadow-lg ${textColorStyles} ${bgColorStyles.contrast}`
 }
 
+export const modalBg = {
+    simple: "w-[100%] lg:w-[calc(100vw-70px+1rem)] h-[calc(100vh-70px)] md:h-[95vh] lg:h-[100vh] top-[70px] left-0 lg:top-0 lg:left-[calc(70px+1rem)] bg-[#232323]/[0.8] z-40 fixed flex flex-col justify-center"
+}
