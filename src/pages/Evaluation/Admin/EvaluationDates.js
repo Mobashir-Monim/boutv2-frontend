5d37aa1ee1cd0e6f4a265f905c91064a32c8f103
Mobashir Monim <mobashirmonim@gmail.com>
2022-08-14 00:02:02 +0600

    Add evaluation admin date inputs

---
+++ src/pages/Evaluation/Admin/EvaluationDates.js
@@ -0,0 +1,46 @@
+import { useEffect } from "react";
+import PrimaryButton from "../../../components/Buttons/PrimaryButton";
+import DateInput from "../../../components/FormInputs/DateInput";
+import CardHeader from "../../../components/Headers/CardHeader";
+import { cardStyles, modalBg } from "../../../utils/styles";
+
+const EvaluationDates = ({ show, toggleDateModal, setStartDate, setEndDate, startDate }) => {
+    const escapeHandler = ({ keyCode }) => {
+        if (keyCode === 27) { toggleDateModal(false); }
+    }
+
+    useEffect(() => {
+        return () => window.addEventListener("keydown", escapeHandler);
+    });
+
+
+    return <div className={`${modalBg.simple} ${show ? "" : "hidden"}`} onClick={() => { toggleDateModal(false) }}>
+        <div className={`${cardStyles.simple} w-[90%] md:w-[70%] z-10 lg:w-[60%] mx-auto p-5`} onClick={event => event.stopPropagation()}>
+            <div className="flex flex-row justify-end"><span className="material-icons-round cursor-pointer" onClick={() => { toggleDateModal(false) }}>close</span></div>
+            <CardHeader title="Evaluation Dates" />
+            <div className="flex flex-col mt-5 md:flex-row w-[100%] gap-5 justify-between">
+                <div className="flex flex-col text-left md:w-[47%]">
+                    <DateInput
+                        name={"start"}
+                        label={"Evaluation Start Date"}
+                        onChangeFn={setStartDate}
+                        minDate={(new Date()).toISOString().split("T")[0]}
+                    />
+                </div>
+                <div className="flex flex-col text-left md:w-[47%]">
+                    <DateInput
+                        name={"end"}
+                        label={"Evaluation End Date"}
+                        onChangeFn={setEndDate}
+                        minDate={startDate}
+                    />
+                </div>
+            </div>
+            <div className="text-right mt-5">
+                <PrimaryButton text={"Confirm Dates"} />
+            </div>
+        </div>
+    </div>
+}
+
+export default EvaluationDates;
--- src/pages/Evaluation/Evaluation.js
+++ src/pages/Evaluation/Evaluation.js
@@ -5,14 +5,24 @@ import SelectInput from "../../components/FormInputs/SelectInput";
 import CardHeader from "../../components/Headers/CardHeader";
 import SimpleCard from "../../components/Card/SimpleCard";
 import { cardStyles } from "../../utils/styles";
+import EvaluationDates from "./Admin/EvaluationDates";
 
 const Evaluation = () => {
     const years = Array((new Date()).getUTCFullYear() - 2022 + 1).fill().map((_, idx) => (new Date()).getUTCFullYear() + idx);
     const semesters = ["Spring", "Summer", "Fall"];
 
     const [pageState, setPageState] = useState({
-        evaluationViewForm: { year: years[0], semester: semesters[0] }
-    })
+        evaluationViewForm: { year: years[0], semester: semesters[0] },
+        evaluationViewDates: { show: false, start: (new Date()).toISOString().split("T")[0], end: null }
+    });
+
+    const toggleEvaluationDatesModal = (show) => {
+        show = typeof (show) === "boolean" ? show : !pageState.evaluationViewDates.show;
+        setPageState({
+            ...pageState,
+            evaluationViewDates: { ...pageState.evaluationViewDates, show: show }
+        })
+    }
 
     const fetchEvaluationSemesterData = event => {
         alert("Feature not complete yet")
@@ -33,6 +43,20 @@ const Evaluation = () => {
         })
     }
 
+    const setStartDate = event => {
+        setPageState({
+            ...pageState,
+            evaluationViewDates: { ...pageState.evaluationViewDates, start: event.target.value }
+        })
+    }
+
+    const setEndDate = event => {
+        setPageState({
+            ...pageState,
+            evaluationViewDates: { ...pageState.evaluationViewDates, end: event.target.value }
+        })
+    }
+
     return <div className="w-[90%] mx-auto min-h-[95vh]">
         <div className="flex flex-col md:flex-row gap-y-10 md:justify-between">
             <form className={`w-[100%] lg:w-[60%] ${cardStyles.simple}`} onSubmit={fetchEvaluationSemesterData}>
@@ -50,15 +74,22 @@ const Evaluation = () => {
                 </div>
             </form>
             <SimpleCard title="Evaluation Admin Checklist" width="w-[100%] lg:w-[35%]">
-                <div className="flex flex-row flex-wrap mt-5 gap-5 mx-auto">
+                <div className="flex flex-row flex-wrap mt-5 justify-between gap-5 mx-auto">
                     <SecondaryButton text={"Set Questions"} link={"/evaluation/questions"} />
-                    <SecondaryButton text={"Set Dates"} link={"/evaluation/dates"} />
+                    <SecondaryButton text={"Set Dates"} clickFunction={toggleEvaluationDatesModal} />
                     <SecondaryButton text={"Set Matrix"} link={"/evaluation/analysis"} />
                     <SecondaryButton text={"Initiate Collection"} link={"/evaluation/initiate"} />
                     <SecondaryButton text={"Publish Results"} link={"/evaluation/publish"} />
                 </div>
             </SimpleCard>
         </div>
+        <EvaluationDates
+            show={pageState.evaluationViewDates.show}
+            toggleDateModal={toggleEvaluationDatesModal}
+            startDate={pageState.evaluationViewDates.start}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+        />
     </div>
 }
 
