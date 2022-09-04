e20234a0ff05ce394d900b078fb44671c554aaae
Mobashir Monim <mobashirmonim@gmail.com>
2022-09-04 10:32:07 +0600

    Update dashboard page

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,6 +1,23 @@
+import PrimaryButton from "../../components/Buttons/PrimaryButton";
+import SimpleCard from "../../components/Card/SimpleCard";
+import { useAuth } from "../../utils/contexts/AuthContext";
+import { pageLayoutStyles } from "../../utils/styles/styles";
+
 const Dashboard = () => {
-    return <div>
-        
+    const { user } = useAuth();
+
+    return <div className={`${pageLayoutStyles.scrollable} flex flex-col justify-center`}>
+        <div className="w-[100%] md:w-[70%] lg:w-[50%] mx-auto">
+            <SimpleCard title={`Hello ${user.displayName}!!`} customStyle="rounded-xl">
+                <div className="mt-5">
+                    <p className="text-center mb-5">What would you like to do today?</p>
+                    <div className="flex flex-row flex-wrap justify-center gap-5">
+                        <PrimaryButton text={"Evaluation Panel"} link="/evaluation" />
+                        <PrimaryButton text={"Student Mapper"} link="/student-mapper" />
+                    </div>
+                </div>
+            </SimpleCard>
+        </div>
     </div>
 }
 
