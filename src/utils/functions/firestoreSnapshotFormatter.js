9a0c0d3db67f22eaa5c41fe2273e874bc868201e
Mobashir Monim <mobashirmonim@gmail.com>
2022-09-07 22:03:33 +0600

    Update firestore snapshot formatter

--- src/utils/functions/firestoreSnapshotFormatter.js
+++ src/utils/functions/firestoreSnapshotFormatter.js
@@ -1,7 +1,12 @@
 export const firestoreSnapshotFormatter = (snapshots, results) => {
     if (snapshots.size > 0) {
+        if (results.length > 0) {
+            if (results[0][1] === null)
+                delete results[0];
+        }
+
         snapshots.forEach(snapshot => { results.push([snapshot.data(), snapshot.id]) });
-    } else {
+    } else if (results.length === 0) {
         results.push([null, null]);
     }
 
