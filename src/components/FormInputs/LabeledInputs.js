95811cd7b011bce5e7e998868ee252585f8170d7
Mobashir Monim <mobashirmonim@gmail.com>
2022-10-07 18:54:26 +0600

    Fix value assignment of labeled inputs

--- src/components/FormInputs/LabeledInputs.js
+++ src/components/FormInputs/LabeledInputs.js
@@ -2,14 +2,14 @@ import { inputStyles } from "../../utils/styles/styles";
 
 export const DateInput = ({ name, label, onChangeFn, minDate, maxDate, value }) => {
     return <div className="flex flex-col">
-        <input type="date" name={name} className={`${inputStyles.labeled.input}`} value={value} onChange={onChangeFn} min={minDate} max={maxDate} />
+        <input type="date" name={name} className={`${inputStyles.labeled.input}`} value={value ? value : ""} onChange={onChangeFn} min={minDate} max={maxDate} />
         <p className={`${inputStyles.labeled.label}`}>{label}</p>
     </div>
 }
 
 export const LineInput = ({ name, label, onChangeFn, value, customStyle = {}, placeholder }) => {
     return <div className={`flex flex-col ${customStyle.container}`}>
-        <input type="text" name={name} className={`${inputStyles.labeled.input} ${customStyle.input}`} onChange={onChangeFn} value={value} placeholder={placeholder} />
+        <input type="text" name={name} className={`${inputStyles.labeled.input} ${customStyle.input}`} onChange={onChangeFn} value={value ? value : ""} placeholder={placeholder} />
         <p className={`${inputStyles.labeled.label} ${customStyle.label}`}>{label}</p>
     </div>
 }
@@ -44,7 +44,7 @@ export const TextInput = ({ label, onChangeFn, value, customStyle = {}, placehol
     }
 
     return <div className={`flex flex-col ${customStyle.container}`}>
-        <textarea type="text" className={`resize-none ${inputStyles.labeled.input} ${customStyle.input} !rounded-xl`} onChange={onChangeFn} onKeyUp={resizeSelf} value={value} placeholder={placeholder} disabled={disabled ? "disabled" : ""} />
+        <textarea type="text" className={`resize-none ${inputStyles.labeled.input} ${customStyle.input} !rounded-xl`} onChange={onChangeFn} onKeyUp={resizeSelf} value={value ? value : ""} placeholder={placeholder} disabled={disabled ? "disabled" : ""} />
         <p className={`${inputStyles.labeled.label}`}>{label}</p>
     </div>
 }
