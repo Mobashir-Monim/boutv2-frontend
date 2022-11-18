fef03e477fd59ca93ccd25d25378dd303132d4c0
Mobashir Monim <mobashirmonim@gmail.com>
2022-11-18 22:30:44 +0600

    Make hook and generalized component for semester selection

---
+++ src/components/Utils/SemesterSelect.js
@@ -0,0 +1,28 @@
+import SimpleCard from "../Card/SimpleCard";
+import { SelectInput } from "../FormInputs/LabeledInputs";
+
+const SemesterSelect = ({ semesters, years, selection, updateSelection }) => {
+    const optionsGenerator = (target, optionsContainer) => selection[target] ?
+        optionsContainer :
+        [`Select ${target.slice(0, 1).toUpperCase()}${target.slice(1)}`, ...optionsContainer];
+
+
+    return <SimpleCard title={"Semester"}>
+        <div className="p-5 flex flex-col gap-5">
+            <SelectInput
+                onChangeFn={event => updateSelection(event.target.value, "semester")}
+                options={optionsGenerator("semester", semesters)}
+                label="Semester"
+                value={selection.semester}
+            />
+            <SelectInput
+                onChangeFn={event => updateSelection(event.target.value, "year")}
+                options={optionsGenerator("year", years)}
+                label="Year"
+                value={selection.year}
+            />
+        </div>
+    </SimpleCard>
+}
+
+export default SemesterSelect;
